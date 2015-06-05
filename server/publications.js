e1dc68a91cc1cdf638873b35c84423fa66ad8616
Nick Riebeek <nick.riebeek@gmail.com>
2015-06-05 16:37:07 -0400

    added parameter check to publications

--- server/publications.js
+++ server/publications.js
@@ -1,7 +1,9 @@
 Meteor.publish('images', function(limit, userSlug) {
+	check(limit, Number);
 
 	var findQuery = {};
 	if (userSlug) {
+		check(userSlug, String);
 		findQuery = { userSlug : userSlug };
 	}
 
